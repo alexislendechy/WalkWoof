a722a17a955bf67601146df770a71dc91712f139
robertako97 <roberto.diazgmx@gmail.com>
2023-11-12 16:20:38 -0800

    Added comments to edit Map later, need to implement db before adding to point distance

--- client/src/components/MapComponent.jsx
+++ client/src/components/MapComponent.jsx
@@ -1,34 +1,41 @@
 import "leaflet/dist/leaflet.css";
 import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 
 const MapComponent = () => {
+  /////////////////////////////////
+  // States
+  /////////////////////////////////
   const [searchText, setSearchText] = useState("");
   const [coordinates, setCoordinates] = useState(null);
   const [mapInitError, setMapInitError] = useState(null);
-
+  /////////////////////////////////
+  // clear map initialization error when coordinates are updated
+  /////////////////////////////////
   useEffect(() => {
     if (coordinates) {
       setMapInitError(null); // Clear any previous map initialization errors
     }
   }, [coordinates]);
-
+  /////////////////////////////////
+  // Function to handle geocoding based on search text
+  /////////////////////////////////
   const handleGeocode = () => {
     const geocoder = new window.google.maps.Geocoder();
     geocoder.geocode({ address: searchText }, (results, status) => {
       if (status === "OK" && results.length > 0) {
         const location = results[0].geometry.location;
-        setCoordinates(location);
-        setMapInitError(null); // Clear any previous map initialization errors
+        setCoordinates(location); // Update state with new coordinates
       } else {
         setCoordinates(null);
-        setMapInitError(`Geocoding failed: ${status}`);
+        setMapInitError(`Geocoding failed: ${status}`); // Set error state if geocoding fails
         console.error("Geocoding failed:", status);
       }
     });
   };
-
+  /////////////////////////////////
+  // Render
+  /////////////////////////////////
   return (
     <div>
       <h1>MAP</h1>
@@ -36,13 +43,14 @@ const MapComponent = () => {
         type="text"
         placeholder="Enter an address or location"
         value={searchText}
-        onChange={(e) => setSearchText(e.target.value)}
+        onChange={(e) => setSearchText(e.target.value)} // Update search text as user types
       />
       <button onClick={handleGeocode}>Go to address!</button>
 
-      {mapInitError ? (
+      {mapInitError ? ( //If map initiation fails
         <p>Error initializing the map: {mapInitError}</p>
-      ) : coordinates ? (
+      ) : //Else
+      coordinates ? ( //Are coordinates available?
         <MapContainer
           center={{ lat: coordinates.lat(), lng: coordinates.lng() }}
           zoom={13}
@@ -59,9 +67,12 @@ const MapComponent = () => {
             </Popup>
           </Marker>
         </MapContainer>
-      ) : null}
+      ) : //End Map render
+      //Else if Map initiation succeed but coordinates are not available render null
+      null}
     </div>
   );
 };
 
+// Exporting MapComponent for use in other parts of the application
 export default MapComponent;
