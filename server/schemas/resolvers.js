dd4f8ccc645374a72697fd7f96f7d90587421b90
rarg08 <rarg08@gmail.com>
2023-11-13 20:54:09 -0600

    missed saved files :P
    i forgot to save some files for the previous push sorry :P

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,28 @@
+const {signToken, AuthenticationError} = require('../utils/auth')
+c
+const resolvers = { 
+    Query: {},
+
+    Mutation: {
+        addUser: async (parent, {username, email, password}) => {
+            const user = await User.create({username, email, password});
+            const token = signToken(user);
+            return {token, user};
+        },
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({email});
+            if(!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return {token, user};
+        },
+        removeUser: async (parent, {username, email}, context) => {
+            if (context.username)
+            return await User.findOneAndDelete({username, email});
+        
+    }
