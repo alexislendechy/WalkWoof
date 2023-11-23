c4f57e13753905f6371c461a0b1c29b12eda314b
robertako97 <roberto.diazgmx@gmail.com>
2023-11-22 17:01:20 -0800

    error

--- client/src/pages/Appointments.jsx
+++ client/src/pages/Appointments.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AppointmentList from "../components/AppointmentList";
+import MapComponentAppointments from "../components/MapComponentAppointments";
 
 const Appointments = () => {
-    return (
-        <div>
-        <AppointmentList />
-        </div>
-    );
+  return (
+    <div>
+      <AppointmentList />
+      <MapComponentAppointments />
+    </div>
+  );
 };
 
 export default Appointments;
